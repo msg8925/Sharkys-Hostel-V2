9b753d8172509e04e259ea2cdecf45f0c38378f2
unknown <msg.8925@gmail.com>
2025-08-12 12:51:31 +0700

    Room model updated.

--- back-end/app/Models/Room.php
+++ back-end/app/Models/Room.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'price_per_night', 'images'];
+
+    public function images()
+    {
+        return $this->hasMany(RoomImage::class);
+    }
 }
+
+
